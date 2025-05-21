deca065e7f031ca8593a24ca73efa9537891b2a0
Jhocep Verastegui <snkje123@gmail.com>
2025-05-21 17:13:23 -0500

    Agregar archivos de proyecto.

---
+++ Sem09/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyejemplClass09;
+
+namespace Sem09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Areas a = new Areas();
+            Console.WriteLine("ingrese el lado: ");
+            double x = double.Parse(Console.ReadLine());
+            double r1 = a.AreaCuadrado(x);
+            Console.WriteLine("El area del cuadrado es: " + r1);
+
+            
+            double r2 = a.AreaCirculo(9);
+            Console.WriteLine("El area del circulo es: " + r2);
+        }
+    }
+}
